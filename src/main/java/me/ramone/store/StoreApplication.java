42d9471317442516e45416c5f803efa4750a39ab
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-23 08:58:44 +0900

    Reverting back to Spring config

--- src/main/java/me/ramone/store/StoreApplication.java
+++ src/main/java/me/ramone/store/StoreApplication.java
@@ -7,16 +7,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-//        SpringApplication.run(StoreApplication.class, args);
-        var orderService = new OrderService(new PayPalPaymentService());
+        SpringApplication.run(StoreApplication.class, args);
 
+        // DEMO CODE
+//        var orderService = new OrderService(new PayPalPaymentService());
         /*
         * PROBLLEM: If we forget to set the payment service we will get an error when we call placeOrder
         * Setter Injection should be used when the dependency is optional
         * It is more common to use Constructor Injection
         * */
+
 //        orderService.setPaymentService(new StripePaymentService());
-        orderService.placeOrder();
+//        orderService.placeOrder();
     }
 
 }
