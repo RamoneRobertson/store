c13dec9690853171f36498c3393e36f4b3b59014
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-23 08:54:56 +0900

    Learning about constructor and setter injection

--- src/main/java/me/ramone/store/OrderService.java
+++ src/main/java/me/ramone/store/OrderService.java
@@ -1,13 +1,20 @@
 package me.ramone.store;
 
 public class OrderService {
+
     private PaymentService paymentService;
 
+    // Constructor Injection
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
     public void placeOrder(){
         paymentService.processPayment(10);
+    }
+
+    // Setter Injection
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 }
--- src/main/java/me/ramone/store/StoreApplication.java
+++ src/main/java/me/ramone/store/StoreApplication.java
@@ -9,6 +9,13 @@ public class StoreApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(StoreApplication.class, args);
         var orderService = new OrderService(new PayPalPaymentService());
+
+        /*
+        * PROBLLEM: If we forget to set the payment service we will get an error when we call placeOrder
+        * Setter Injection should be used when the dependency is optional
+        * It is more common to use Constructor Injection
+        * */
+//        orderService.setPaymentService(new StripePaymentService());
         orderService.placeOrder();
     }
 
