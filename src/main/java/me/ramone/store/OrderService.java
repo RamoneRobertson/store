26eacb4f96c1ef384a6dc7acd40787847417ec35
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-24 16:41:41 +0900

    Added different payment services for paypal and stripe

--- src/main/java/me/ramone/store/OrderService.java
+++ src/main/java/me/ramone/store/OrderService.java
@@ -1,10 +1,17 @@
 package me.ramone.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 
     private PaymentService paymentService;
 
     // Constructor Injection
+    @Autowired
+    // lets Spring hand your class the tools it needs, instead of your class building those tools itself
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
--- src/main/java/me/ramone/store/StoreApplication.java
+++ src/main/java/me/ramone/store/StoreApplication.java
@@ -2,23 +2,26 @@ package me.ramone.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
 
-        // DEMO CODE
-//        var orderService = new OrderService(new PayPalPaymentService());
-        /*
-        * PROBLLEM: If we forget to set the payment service we will get an error when we call placeOrder
-        * Setter Injection should be used when the dependency is optional
-        * It is more common to use Constructor Injection
-        * */
-
-//        orderService.setPaymentService(new StripePaymentService());
-//        orderService.placeOrder();
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+        var orderService = context.getBean(OrderService.class);
+        orderService.placeOrder();
     }
 
 }
+
+// DEMO CODE
+/*
+ * PROBLLEM: If we forget to set the payment service we will get an error when we call placeOrder
+ * Setter Injection should be used when the dependency is optional
+ * It is more common to use Constructor Injection
+ * */
+
+//        orderService.setPaymentService(new StripePaymentService());
+//        var orderService = new OrderService(new PayPalPaymentService());
