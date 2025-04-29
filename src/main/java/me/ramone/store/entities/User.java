c5839a7dc42b9405be4fb8f352da5ae13956fe9c
Ramone Robertson <ramonerobertson10@gmail.com>
2025-04-29 20:55:32 +0900

    Adding builder default annotation for address list in User class

--- src/main/java/me/ramone/store/entities/User.java
+++ src/main/java/me/ramone/store/entities/User.java
@@ -30,6 +30,7 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user")
+    @Builder.Default
     private List<Address> addresses = new ArrayList<>();
 
     public void addAdress(Address address){
